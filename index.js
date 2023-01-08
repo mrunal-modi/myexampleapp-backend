f9b5006966b15501d02085fcef80b5628a8bf632
mrunal-modi <mrunal.modi@gmail.com>
2023-01-08 16:48:46 +0530

    mm

--- index.js
+++ index.js
@@ -1,35 +1,103 @@
-// Imports
-const express = require('express');
-const cors    = require('cors');
-const expressValidator = require("express-validator");
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-dotenv.config();
+var express = require('express');
+var app     = express();
+var cors    = require('cors');
+var dal     = require('./dal.js');
+const e = require('express');
 
-// API Routes
-const routes = require('./src/routes');
-
-// DB
-require('./src/db');
-
-// Express Middleware + allow cross-origin resource sharing (CORS)
-const app     = express();
+// used to serve static files from public directory
+app.use(express.static('public'));
 app.use(cors());
 
-// Data parser - used to parse post data
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(expressValidator());
+// create user 
+app.get('/account/create/:name/:email/:password', function (req, res) {
 
-// Routes
-app.use("/api/", routes);
+    // check if account exists
+    dal.find(req.params.email).
+        then((users) => {
 
-// start server
-// -----------------------
-var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-app.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+            // if user exists, return error message
+            if(users.length > 0){
+                console.log('User already in exists');
+                res.send('User already in exists');    
+            }
+            else{
+                // else create user
+                dal.create(req.params.name,req.params.email,req.params.password).
+                    then((user) => {
+                        console.log(user);
+                        res.send(user);            
+                    });            
+            }
+
+        });
 });
 
 
+// login user 
+app.get('/account/login/:email/:password', function (req, res) {
+
+    dal.find(req.params.email).
+        then((user) => {
+
+            // if user exists, check password
+            if(user.length > 0){
+                if (user[0].password === req.params.password){
+                    res.send(user[0]);
+                }
+                else{
+                    res.send('Login failed: wrong password');
+                }
+            }
+            else{
+                res.send('Login failed: user not found');
+            }
+    });
+    
+});
+
+// find user account
+app.get('/account/find/:email', function (req, res) {
+
+    dal.find(req.params.email).
+        then((user) => {
+            console.log(user);
+            res.send(user);
+    });
+});
+
+// find one user by email - alternative to find
+app.get('/account/findOne/:email', function (req, res) {
+
+    dal.findOne(req.params.email).
+        then((user) => {
+            console.log(user);
+            res.send(user);
+    });
+});
+
+
+// update - deposit/withdraw amount
+app.get('/account/update/:email/:amount', function (req, res) {
+
+    var amount = Number(req.params.amount);
+
+    dal.update(req.params.email, amount).
+        then((response) => {
+            console.log(response);
+            res.send(response);
+    });    
+});
+
+// all accounts
+app.get('/account/all', function (req, res) {
+
+    dal.all().
+        then((docs) => {
+            console.log(docs);
+            res.send(docs);
+    });
+});
+
+var port = 3000;
+app.listen(port);
+console.log('Running on port: ' + port);
